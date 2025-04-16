1d5c0c45106f5bd20c6a37b5a8d6f71486bcfc12
Gusradika <oktaven.soft@gmail.com>
2025-04-16 23:30:40 +0700

    nuxt layout

---
+++ app/layouts/blue.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="layout-container">
+    <main class="main-content">
+      <!-- Content will be Rendered in Slot -->
+      <slot />
+    </main>
+  </div>
+</template>
+
+<style scoped>
+.layout-container {
+  height: calc(100vh - 4rem);
+  background-color: var(--ui-bg);
+}
+
+.main-content {
+  height: 100%;
+  margin-top: 4rem;
+}
+</style>
--- app/pages/chat.vue
+++ app/pages/chat.vue
@@ -1,3 +1,16 @@
 <template>
-  <h1>This is chat</h1>
+  <div>
+    <!-- Overriding. make sure definePageMeta 'layout' set to 'false' -->
+    <NuxtLayout name="blue">
+      <h1>This is chat</h1>
+    </NuxtLayout>
+  </div>
 </template>
+
+<!-- Layout Setup per Page -->
+<script setup lang="ts">
+definePageMeta({
+  layout: "blue",
+  //   false -> not using layout
+});
+</script>
